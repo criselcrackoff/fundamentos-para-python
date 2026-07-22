18c9299a0e760272644e139ae08408502aadff69
criselcrackoff <criselcrack.off@gmail.com>
2026-07-22 10:20:30 -0400

    Agregada Ruta /celulares + diccionario simple

--- ejercicios_03.py
+++ ejercicios_03.py
@@ -5,3 +5,23 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/celulares")  # Ruta/Path/Endpoint
+async def obtenerCelulares():
+    return [
+        { 
+            "id": 1, 
+            "marca": "APPLE",
+            "modelo": "iPhone 17 pro max"
+        },
+        {
+            "id": 2, 
+            "marca": "Samsung",
+            "modelo": "Samsung Galaxy S25 Ultra"
+        },
+        {
+            "id": 2, 
+            "marca": "Motorola",
+            "modelo": "Motorola Edge 50"
+        }
+    ]
